8eb4b6453167c92f84c17541f3325406548ffff6
NathalyAmaral <nathalyamaral07@gmail.com>
2019-03-17 17:39:35 -0300

    alterações template

---
+++ template_store/index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<?php
+			require_once("head.html")
+		?>
+	</head>
+	<body>
+		
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-2">
+							<div id="colorlib-logo"><a href="index.php">Use New Mic</a></div>
+						</div>
+						<div class="col-xs-10 text-right menu-1">
+							<ul>
+								<li class="active"><a href="index.html">Home</a></li>
+								<li><a href="shop.html">Produtos</a></li>
+								<li><a href="cart.html"><i class="icon-shopping-cart"></i> Carrinho </a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<aside id="colorlib-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/img_bg_1.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-md-pull-2 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner">
+				   					<div class="desc"><br>
+					   					<h1 class="head-1">Masculino</h1>
+					   					<h2 class="head-2">Jeans</h2>
+					   					<h2 class="head-3">Colecção</h2> <br>
+					   					<p><a href="#" class="btn btn-primary">Confira a coleção</a></p>
+					   					</div>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_2.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-md-pull-2 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner">
+				   					<div class="desc"><br>
+					   					<h1 class="head-1">Feminino</h1>
+					   					<h2 class="head-2">Venda</h2>
+					   					<h2 class="head-3">45% desconto</h2><br>
+					   					<p><a href="#" class="btn btn-primary">Confira a coleção</a></p>
+					   					</div>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_3.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-md-push-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner">
+				   					<div class="desc">
+					   					<h1 class="head-1">Novo</h1>
+					   					<h2 class="head-2">Coleção Abril</h2>
+					   					<h2 class="head-3">com 30% <br> de desconto</h2>
+					   					<p><a href="#" class="btn btn-primary">Confira a coleção</a></p>
+				   					</div>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			  	</ul>
+		  	</div>
+		</aside>
+		<div id="colorlib-featured-product">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<a href="shop.html" class="f-product-1" style="background-image: url(images/item-1.jpg);">
+							<div class="desc">
+								<h2>Moda <br>para <br>homens</h2>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-6">
+						<div class="row">
+							<div class="col-md-6">
+								<a href="" class="f-product-2" style="background-image: url(images/item-2.jpg);">
+									<div class="desc">
+										<h2>Novos <br>Vestidos</h2>
+									</div>
+								</a>
+							</div>
+							<div class="col-md-6">
+								<a href="" class="f-product-2" style="background-image: url(images/item-4.jpg);">
+									<div class="desc">
+										<h2>Com <br>20% <br>desconto</h2>
+									</div>
+								</a>
+							</div>
+							<div class="col-md-12">
+								<a href="" class="f-product-2" style="background-image: url(images/item-3.jpg);">
+									<div class="desc">
+										<h2>Tênis <br>para <br>homens</h2>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="colorlib-shop">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center colorlib-heading">
+						<h2><span>Nova Coleção Abril</span></h2>
+						<p> Uma das características da moda são o ciclo e reciclagem de tendências de décadas anteriores, a moda está sempre passeando e se inspirando em épocas marcantes do passado e nesse verão 2019 não vai ser diferente, vem aí incríveis tendências que vão fazer você vibrar e celebrar a estação mais querida por todos. Veja a seguir</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-3 text-center">
+						<div class="product-entry">
+							<div class="product-img" style="background-image: url(images/item-5.jpg);">
+								<p class="tag"><span class="new">Novo</span></p>
+								<div class="cart">
+									<p>
+										<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
+										<span><a href="product-detail.html"><i class="icon-eye"></i></a></span> 
+										<span><a href="#"><i class="icon-heart3"></i></a></span>
+										<span><a href="add-to-wishlist.html"><i class="icon-bar-chart"></i></a></span>
+									</p>
+								</div>
+							</div>
+							<div class="desc">
+								<h3><a href="shop.html">Vestido Floral</a></h3>
+								<p class="price"><span>R$120.00</span></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 text-center">
+						<div class="product-entry">
+							<div class="product-img" style="background-image: url(images/item-6.jpg);">
+								<p class="tag"><span class="new">Novo</span></p>
+								<div class="cart">
+									<p>
+										<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
+										<span><a href="product-detail.html"><i class="icon-eye"></i></a></span> 
+										<span><a href="#"><i class="icon-heart3"></i></a></span>
+										<span><a href="add-to-wishlist.html"><i class="icon-bar-chart"></i></a></span>
+									</p>
+								</div>
+							</div>
+							<div class="desc">
+								<h3><a href="shop.html">Camisa branca Aesthetic</a></h3>
+								<p class="price"><span>R$80.00</span></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 text-center">
+						<div class="product-entry">
+							<div class="product-img" style="background-image: url(images/item-7.jpg);">
+								<p class="tag"><span class="new">Novo</span></p>
+								<div class="cart">
+									<p>
+										<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
+										<span><a href="product-detail.html"><i class="icon-eye"></i></a></span> 
+										<span><a href="#"><i class="icon-heart3"></i></a></span>
+										<span><a href="add-to-wishlist.html"><i class="icon-bar-chart"></i></a></span>
+									</p>
+								</div>
+							</div>
+							<div class="desc">
+								<h3><a href="shop.html">Conjunto jeans + camisa</a></h3>
+								<p class="price"><span>R$180.00</span></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 text-center">
+						<div class="product-entry">
+							<div class="product-img" style="background-image: url(images/item-8.jpg);">
+								<p class="tag"><span class="new">Novo</span></p>
+								<div class="cart">
+									<p>
+										<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
+										<span><a href="product-detail.html"><i class="icon-eye"></i></a></span> 
+										<span><a href="#"><i class="icon-heart3"></i></a></span>
+										<span><a href="add-to-wishlist.html"><i class="icon-bar-chart"></i></a></span>
+									</p>
+								</div>
+							</div>
+							<div class="desc">
+								<h3><a href="shop.html">Conjunto Yellow</a></h3>
+								<p class="price"><span>R$300.00</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="colorlib-intro" class="colorlib-intro" style="background-image: url(images/cover-img-1.jpg);" data-stellar-background-ratio="0.5">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="intro-desc">
+							<div class="text-salebox">
+								<div class="text-lefts">
+									<div class="sale-box">
+										<div class="sale-box-top">
+											<h2 class="number">45</h2>
+											<span class="sup-1">%</span>
+											<span class="sup-2">DE</span>
+										</div>
+										<h2 class="text-sale">DESCONTO</h2>
+									</div>
+								</div>
+								<div class="text-rights">
+									<h3 class="title">Quantidade limitada!</h3>
+									<p>Separe já quantas peças quiser.</p>
+									<p><a href="shop.html" class="btn btn-primary">Comprar agora</a> 
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		
+
+		<div id="colorlib-testimony" class="colorlib-light-grey">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center colorlib-heading">
+						<h2><span>Depoimentos de clientes</span></h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">						
+						<div class="owl-carousel2">
+							<div class="item">
+								<div class="testimony text-center">
+									<span class="img-user" style="background-image: url(images/person1.jpg);"></span>
+									<span class="user">Luiza</span>
+									<small>Corumbá, MS</small>
+									<blockquote>
+										<p>"Pude renovar meu guarda roupa".</p>
+									</blockquote>
+								</div>
+							</div>
+							<div class="item">
+								<div class="testimony text-center">
+									<span class="img-user" style="background-image: url(images/person3.jpg);"></span>
+									<span class="user">Jenifer</span>
+									<small>Corumbá, MS</small>
+									<blockquote>
+										<p>"Ótimas roupas para praticar esportes".</p>
+									</blockquote>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+
+		
+		
+		<div id="colorlib-subscribe">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="col-md-6 text-center">
+							<h2><i class="icon-paperplane"></i>Receba nossas ofertas</h2>
+						</div>
+						<div class="col-md-6">
+							<form class="form-inline qbstp-header-subscribe">
+								<div class="row">
+									<div class="col-md-12 col-md-offset-0">
+										<div class="form-group">
+											<input type="text" class="form-control" id="email" placeholder="Digite seu email">
+											<button type="submit" class="btn btn-primary">Increver-se</button>
+										</div>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		
+		<?php
+			require_once("footer.html")
+		?>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+
+	</body>
+</html>
+
