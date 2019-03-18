64615a29e6b073d94aa5b3e75bd7241cf3656a65
NathalyAmaral <nathalyamaral07@gmail.com>
2019-03-18 11:11:24 -0300

    Update

---
+++ template_store/login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<?php
+			include_once("head.html")
+		?>
+	</head>
+	<body>
+		
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-2">
+							<div id="colorlib-logo"><a href="index.php">Use New Mic</a></div>
+						</div>
+						<div class="col-xs-10 text-right menu-1">
+							<ul>
+								<li><a href="index.php">Home</a></li>
+								<li class="active"><a href="shop.php">Produtos</a></li>
+								<li><a href="login.php"> Login/Cadastre-se </a></li>
+								<li><a href="cart.php"><i class="icon-shopping-cart"></i> Carrinho </a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+		<div class="colorlib-shop">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-md-push-2">
+					</div>
+					
+			</div>
+				
+		</div>
+
+		<?php
+			include_once("footer.html");
+		?>
+	</div>
+
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+	
+	</body>
+</html>
+
--- template_store/shop.php
+++ template_store/shop.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<?php
-			require_once("head.html")
+			include_once("head.html")
 		?>
 	</head>
 	<body>
@@ -38,8 +38,8 @@
 			   			<div class="row">
 				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
 				   				<div class="slider-text-inner text-center">
-				   					<h1>Products</h1>
-				   					<h2 class="bread"><span><a href="index.html">Home</a></span> <span>Shop</span></h2>
+				   					<h1>Produtos</h1>
+				   					<h2 class="bread"><span><a href="index.html">Home</a></span> <span>Produtos</span></h2>
 				   				</div>
 				   			</div>
 				   		</div>
@@ -57,7 +57,7 @@
 							<div class="col-md-4 text-center">
 								<div class="product-entry">
 									<div class="product-img" style="background-image: url(images/item-5.jpg);">
-										<p class="tag"><span class="new">New</span></p>
+										<p class="tag"><span class="new">novo</span></p>
 										<div class="cart">
 											<p>
 												<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
@@ -76,7 +76,7 @@
 							<div class="col-md-4 text-center">
 								<div class="product-entry">
 									<div class="product-img" style="background-image: url(images/item-6.jpg);">
-										<p class="tag"><span class="sale">Sale</span></p>
+										<p class="tag"><span class="sale">desconto</span></p>
 										<div class="cart">
 											<p>
 												<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
@@ -95,7 +95,7 @@
 							<div class="col-md-4 text-center">
 								<div class="product-entry">
 									<div class="product-img" style="background-image: url(images/item-7.jpg);">
-										<p class="tag"><span class="new">New</span></p>
+										<p class="tag"><span class="new">novo</span></p>
 										<div class="cart">
 											<p>
 												<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
@@ -132,7 +132,7 @@
 							<div class="col-md-4 text-center">
 								<div class="product-entry">
 									<div class="product-img" style="background-image: url(images/item-9.jpg);">
-										<p class="tag"><span class="sale">Sale</span></p>
+										<p class="tag"><span class="sale">desconto</span></p>
 										<div class="cart">
 											<p>
 												<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
@@ -151,7 +151,7 @@
 							<div class="col-md-4 text-center">
 								<div class="product-entry">
 									<div class="product-img" style="background-image: url(images/item-10.jpg);">
-										<p class="tag"><span class="new">New</span></p>
+										<p class="tag"><span class="new">novo</span></p>
 										<div class="cart">
 											<p>
 												<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
@@ -188,7 +188,7 @@
 							<div class="col-md-4 text-center">
 								<div class="product-entry">
 									<div class="product-img" style="background-image: url(images/item-12.jpg);">
-										<p class="tag"><span class="new">New</span></p>
+										<p class="tag"><span class="new">novo</span></p>
 										<div class="cart">
 											<p>
 												<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
@@ -207,7 +207,7 @@
 							<div class="col-md-4 text-center">
 								<div class="product-entry">
 									<div class="product-img" style="background-image: url(images/item-13.jpg);">
-										<p class="tag"><span class="new">New</span></p>
+										<p class="tag"><span class="new">novo</span></p>
 										<div class="cart">
 											<p>
 												<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
@@ -226,7 +226,7 @@
 							<div class="col-md-4 text-center">
 								<div class="product-entry">
 									<div class="product-img" style="background-image: url(images/item-14.jpg);">
-										<p class="tag"><span class="new">New</span></p>
+										<p class="tag"><span class="new">novo</span></p>
 										<div class="cart">
 											<p>
 												<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
@@ -245,7 +245,7 @@
 							<div class="col-md-4 text-center">
 								<div class="product-entry">
 									<div class="product-img" style="background-image: url(images/item-15.jpg);">
-										<p class="tag"><span class="new">New</span></p>
+										<p class="tag"><span class="new">novo</span></p>
 										<div class="cart">
 											<p>
 												<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
@@ -264,7 +264,7 @@
 							<div class="col-md-4 text-center">
 								<div class="product-entry">
 									<div class="product-img" style="background-image: url(images/item-16.jpg);">
-										<p class="tag"><span class="new">New</span></p>
+										<p class="tag"><span class="new">novo</span></p>
 										<div class="cart">
 											<p>
 												<span class="addtocart"><a href="cart.html"><i class="icon-shopping-cart"></i></a></span> 
@@ -286,165 +286,17 @@
 								<ul class="pagination">
 									<li class="disabled"><a href="#">&laquo;</a></li>
 									<li class="active"><a href="#">1</a></li>
-									<li><a href="#">2</a></li>
-									<li><a href="#">3</a></li>
-									<li><a href="#">4</a></li>
 									<li><a href="#">&raquo;</a></li>
 								</ul>
 							</div>
 						</div>
 					</div>
-					<div class="col-md-2 col-md-pull-10">
-						<div class="sidebar">
-							<div class="side">
-								<h2>Categories</h2>
-								<div class="fancy-collapse-panel">
-			                  <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-			                     <div class="panel panel-default">
-			                         <div class="panel-heading" role="tab" id="headingOne">
-			                             <h4 class="panel-title">
-			                                 <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Men
-			                                 </a>
-			                             </h4>
-			                         </div>
-			                         <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-			                             <div class="panel-body">
-			                                 <ul>
-			                                 	<li><a href="#">Jeans</a></li>
-			                                 	<li><a href="#">T-Shirt</a></li>
-			                                 	<li><a href="#">Jacket</a></li>
-			                                 	<li><a href="#">Shoes</a></li>
-			                                 </ul>
-			                             </div>
-			                         </div>
-			                     </div>
-			                     <div class="panel panel-default">
-			                         <div class="panel-heading" role="tab" id="headingTwo">
-			                             <h4 class="panel-title">
-			                                 <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Women
-			                                 </a>
-			                             </h4>
-			                         </div>
-			                         <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-			                             <div class="panel-body">
-			                                <ul>
-			                                 	<li><a href="#">Jeans</a></li>
-			                                 	<li><a href="#">T-Shirt</a></li>
-			                                 	<li><a href="#">Jacket</a></li>
-			                                 	<li><a href="#">Shoes</a></li>
-			                                 </ul>
-			                             </div>
-			                         </div>
-			                     </div>
-			                     <div class="panel panel-default">
-			                         <div class="panel-heading" role="tab" id="headingThree">
-			                             <h4 class="panel-title">
-			                                 <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Jewelry
-			                                 </a>
-			                             </h4>
-			                         </div>
-			                         <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-			                             <div class="panel-body">
-			                                <ul>
-			                                 	<li><a href="#">Jeans</a></li>
-			                                 	<li><a href="#">T-Shirt</a></li>
-			                                 	<li><a href="#">Jacket</a></li>
-			                                 	<li><a href="#">Shoes</a></li>
-			                                 </ul>
-			                             </div>
-			                         </div>
-			                     </div>
-			                     <div class="panel panel-default">
-			                         <div class="panel-heading" role="tab" id="headingFour">
-			                             <h4 class="panel-title">
-			                                 <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseThree">Jewelry
-			                                 </a>
-			                             </h4>
-			                         </div>
-			                         <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
-			                             <div class="panel-body">
-			                                <ul>
-			                                 	<li><a href="#">Jeans</a></li>
-			                                 	<li><a href="#">T-Shirt</a></li>
-			                                 	<li><a href="#">Jacket</a></li>
-			                                 	<li><a href="#">Shoes</a></li>
-			                                 </ul>
-			                             </div>
-			                         </div>
-			                     </div>
-			                  </div>
-			               </div>
-							</div>
-							<div class="side">
-								<h2>Price Range</h2>
-								<form method="post" class="colorlib-form-2">
-				              	<div class="row">
-				                <div class="col-md-12">
-				                  <div class="form-group">
-				                    <label for="guests">Price from:</label>
-				                    <div class="form-field">
-				                      <i class="icon icon-arrow-down3"></i>
-				                      <select name="people" id="people" class="form-control">
-				                        <option value="#">1</option>
-				                        <option value="#">200</option>
-				                        <option value="#">300</option>
-				                        <option value="#">400</option>
-				                        <option value="#">1000</option>
-				                      </select>
-				                    </div>
-				                  </div>
-				                </div>
-				                <div class="col-md-12">
-				                  <div class="form-group">
-				                    <label for="guests">Price to:</label>
-				                    <div class="form-field">
-				                      <i class="icon icon-arrow-down3"></i>
-				                      <select name="people" id="people" class="form-control">
-				                        <option value="#">2000</option>
-				                        <option value="#">4000</option>
-				                        <option value="#">6000</option>
-				                        <option value="#">8000</option>
-				                        <option value="#">10000</option>
-				                      </select>
-				                    </div>
-				                  </div>
-				                </div>
-				              </div>
-				            </form>
-							</div>
-							<div class="side">
-								<h2>Color</h2>
-								<div class="color-wrap">
-									<p class="color-desc">
-										<a href="#" class="color color-1"></a>
-										<a href="#" class="color color-2"></a>
-										<a href="#" class="color color-3"></a>
-										<a href="#" class="color color-4"></a>
-										<a href="#" class="color color-5"></a>
-									</p>
-								</div>
-							</div>
-							<div class="side">
-								<h2>Size</h2>
-								<div class="size-wrap">
-									<p class="size-desc">
-										<a href="#" class="size size-1">xs</a>
-										<a href="#" class="size size-2">s</a>
-										<a href="#" class="size size-3">m</a>
-										<a href="#" class="size size-4">l</a>
-										<a href="#" class="size size-5">xl</a>
-										<a href="#" class="size size-5">xxl</a>
-									</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+					
 			</div>
 		</div>
 
 		<?php
-			require_once("footer.html")
+			include_once("footer.html");
 		?>
 	</div>
 
