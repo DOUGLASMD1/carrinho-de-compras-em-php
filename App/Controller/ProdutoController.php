aaaed8ebc1423e1c2119b036526a89cba066ee5a
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2019-03-18 13:53:23 -0400

    Controller Produto.

--- App/Controller/ProdutoController.php
+++ App/Controller/ProdutoController.php
@@ -1,8 +1,9 @@
 <?php
+include_once '../DataBase/conexao.php';
 
 class ProdutoController {
 
-    public function allProdutos(){
+    public static function allProdutos(){
         $conexao = new Conexao();
         $conexao = $conexao->conexao();
         $stmt = $conexao->prepare("SELECT * FROM produto;");
@@ -10,18 +11,6 @@ class ProdutoController {
         $produtos = $stmt->fetchAll();
         $stmt = null;
         return $produtos;
-    }
-
-    public static function cadastrar($dados) {
-        $conexao = new Conexao();
-        $conexao = $conexao->conexao();
-        $stmt = $conexao->prepare("INSERT INTO aluno(nome, bim_1, bim_2, bim_3, bim_4) VALUES(:nome, :nota1, :nota2, :nota3, :nota4);");
-        $stmt->bindParam('nome', $dados['nome']);
-        $stmt->bindParam('nota1', $dados['nota1']);
-        $stmt->bindParam('nota2', $dados['nota2']);
-        $stmt->bindParam('nota3', $dados['nota3']);
-        $stmt->bindParam('nota4', $dados['nota4']);
-        $stmt->execute();
     }
 }
 
