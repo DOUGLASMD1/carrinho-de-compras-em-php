aa5c67488ada9d8529333f116951d05884b9f03f
NathalyAmaral <nathalyamaral07@gmail.com>
2019-03-18 23:19:54 -0300

    index.php

--- template_store/index.php
+++ template_store/index.php
@@ -32,68 +32,64 @@
 		<aside id="colorlib-hero">
 			<div class="flexslider">
 				<ul class="slides">
-			   	<li style="background-image: url(images/img_bg_1.jpg);">
-			   		<div class="overlay"></div>
-			   		<div class="container-fluid">
-			   			<div class="row">
-				   			<div class="col-md-6 col-md-offset-3 col-md-pull-2 col-sm-12 col-xs-12 slider-text">
-				   				<div class="slider-text-inner">
-				   					<div class="desc"><br>
-					   					<h1 class="head-1">Masculino</h1>
-					   					<h2 class="head-2">Jeans</h2>
-					   					<h2 class="head-3">Colecção</h2> <br>
-					   					<p><a href="shop.php" class="btn btn-primary">Confira a coleção</a></p>
+				   	<li style="background-image: url(images/6.jpg);">
+				   		<div class="overlay"></div>
+				   		<div class="container-fluid">
+				   			<div class="row">
+					   			<div class="col-md-6 col-md-offset-3 col-md-pull-2 col-sm-12 col-xs-12 slider-text">
+					   				<div class="slider-text-inner">
+					   					<div class="desc"><br>
+						   					<h1 class="head-1" style="font-size: 35px">Masculino</h1>
+						   					<h2 class="head-2">Jeans</h2>
+						   					<h2 class="head-3" style="font-size: 20px">Colecção</h2> <br>
+						   					<p><a href="shop.php" class="btn btn-primary">Confira a coleção</a></p>
+						   					</div>
+					   				</div>
+					   			</div>
+					   		</div>
+				   		</div>
+				   	</li>
+				   	<li style="background-image: url(images/7.jpg);">
+				   		<div class="overlay"></div>
+				   		<div class="container-fluid">
+				   			<div class="row">
+					   			<div class="col-md-6 col-md-offset-3 col-md-pull-2 col-sm-12 col-xs-12 slider-text">
+					   				<div class="slider-text-inner">
+					   					<div class="desc"><br>
+						   					<h1 class="head-1" style="font-size: 35px">Feminino</h1>
+						   					<h2 class="head-2">Venda</h2>
+						   					<h2 class="head-3" style="font-size: 20px">45% desconto</h2><br>
+						   					<p><a href="shop.php" class="btn btn-primary">Confira a coleção</a></p>
+						   					</div>
+					   				</div>
+					   			</div>
+					   		</div>
+				   		</div>
+				   	</li>
+				   	<li style="background-image: url(images/8.jpg);">
+				   		<div class="overlay"></div>
+				   		<div class="container-fluid">
+				   			<div class="row">
+					   			<div class="col-md-6 col-md-offset-3 col-md-push-3 col-sm-12 col-xs-12 slider-text">
+					   				<div class="slider-text-inner">
+					   					<div class="desc">
+						   					<h1 class="head-1" style="font-size: 35px">Novo</h1>
+						   					<h2 class="head-2">Coleção <br>Abril</h2>
+						   					<h2 class="head-3" style="font-size: 20px">com 30% <br> de desconto</h2>
+						   					<p><a href="shop.php" class="btn btn-primary">Confira a coleção</a></p>
 					   					</div>
-				   				</div>
-				   			</div>
+					   				</div>
+					   			</div>
+					   		</div>
 				   		</div>
-			   		</div>
-			   	</li>
-			   	<li style="background-image: url(images/img_bg_2.jpg);">
-			   		<div class="overlay"></div>
-			   		<div class="container-fluid">
-			   			<div class="row">
-				   			<div class="col-md-6 col-md-offset-3 col-md-pull-2 col-sm-12 col-xs-12 slider-text">
-				   				<div class="slider-text-inner">
-				   					<div class="desc"><br>
-					   					<h1 class="head-1">Feminino</h1>
-					   					<h2 class="head-2">Venda</h2>
-					   					<h2 class="head-3">45% desconto</h2><br>
-					   					<p><a href="shop.php" class="btn btn-primary">Confira a coleção</a></p>
-					   					</div>
-				   				</div>
-				   			</div>
-				   		</div>
-			   		</div>
-			   	</li>
-			   	<li style="background-image: url(images/img_bg_3.jpg);">
-			   		<div class="overlay"></div>
-			   		<div class="container-fluid">
-			   			<div class="row">
-				   			<div class="col-md-6 col-md-offset-3 col-md-push-3 col-sm-12 col-xs-12 slider-text">
-				   				<div class="slider-text-inner">
-				   					<div class="desc">
-					   					<h1 class="head-1">Novo</h1>
-					   					<h2 class="head-2">Coleção Abril</h2>
-					   					<h2 class="head-3">com 30% <br> de desconto</h2>
-					   					<p><a href="shop.php" class="btn btn-primary">Confira a coleção</a></p>
-				   					</div>
-				   				</div>
-				   			</div>
-				   		</div>
-			   		</div>
-			   	</li>
+				   	</li>
 			  	</ul>
 		  	</div>
+		  	
+			<?php
+				include_once("footer.html")
+			?>
 		</aside>
-
-		<?php
-			include_once("footer.html")
-		?>
-	</div>
-
-	<div class="gototop js-top">
-		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
 	</div>
 
 	</body>
