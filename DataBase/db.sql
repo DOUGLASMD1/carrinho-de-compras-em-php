caf71aca4c71989a74e38d72b43cfbdbe1e7712e
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2019-03-22 10:20:56 -0400

    Insert Produtos.

--- DataBase/db.sql
+++ DataBase/db.sql
@@ -165,6 +165,13 @@ CREATE TABLE IF NOT EXISTS `carrinhoCompras`.`finalizaCompra` (
 ENGINE = InnoDB;
 
 
+insert into produto (nome, descricao, valor, imagem) values ('Camisa Branca Aesthetic', 'Camisa algodao, branca lisa', 56.90, 14);
+insert into produto (nome, descricao, valor, imagem) values ('Palito', 'Conjunto Terno + Calca', 250.90, 17);
+insert into produto (nome, descricao, valor, imagem) values ('Conjunto Yellow', 'Moletom e calça algodao', 346.90, 16);
+insert into produto (nome, descricao, valor, imagem) values ('Colegial 2000', 'Blusa rose + Saia Xadrez', 146.90, 13);
+insert into produto (nome, descricao, valor, imagem) values ('Gotic', 'Blusa + Colete + Calça', 223.90, 21);
+insert into produto (nome, descricao, valor, imagem) values ('Vestido Azul Floral', 'Vestido social', 88.90, 23);
+
 Insert into estado (Uf, Nome) values ('AC', 'Acre');
 Insert into estado (Uf, Nome) values ('AL', 'Alagoas');
 Insert into estado (Uf, Nome) values ('AP', 'Amapá');
--- DataBase/insert_produtos.sql
+++
@@ -1,9 +0,0 @@
-
-use carrinhocompras;
-
-insert into produto (nome, descricao, valor, imagem) values ('Camisa Branca Aesthetic', 'Camisa algodao, branca lisa', 56.90, 14);
-insert into produto (nome, descricao, valor, imagem) values ('Palito', 'Conjunto Terno + Calca', 250.90, 17);
-insert into produto (nome, descricao, valor, imagem) values ('Conjunto Yellow', 'Moletom e calça algodao', 346.90, 16);
-insert into produto (nome, descricao, valor, imagem) values ('Colegial 2000', 'Blusa rose + Saia Xadrez', 146.90, 13);
-insert into produto (nome, descricao, valor, imagem) values ('Gotic', 'Blusa + Colete + Calça', 223.90, 21);
-insert into produto (nome, descricao, valor, imagem) values ('Vestido Azul Floral', 'Vestido social', 88.90, 23);
