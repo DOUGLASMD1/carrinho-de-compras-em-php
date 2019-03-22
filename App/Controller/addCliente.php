01d7e1810f4b76b15b8879610464e0e61bd69713
NathalyAmaral <nathalyamaral07@gmail.com>
2019-03-22 15:32:14 -0300

    Validando email, removida mensagem de erro após cadastro

--- App/Controller/addCliente.php
+++ App/Controller/addCliente.php
@@ -2,20 +2,21 @@
 
 		include_once '../../DataBase/conexao.php';
 		include_once 'ClienteController.php';
-		
-		$cliente = new ClienteController();
-		$result = $cliente->cadastrarCliente($_POST);
 
-		if ($result){
+		function addCliente($dados){
+			$cliente = new ClienteController();
+			$result = $cliente->cadastrarCliente($_POST);
 
-			echo "
-				<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=../../template_store/login.php'>
-				<script type=\"text/javascript\">
-					alert(\"Cadastro realizado com sucesso!\");
-				</script>
-				";
-		}else{
-    		echo "Erro ao cadastrar";
-    		$result->errorInfo();
+			if ($result){
+				echo "
+					<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=../../template_store/login.php'>
+					<script type=\"text/javascript\">
+						alert(\"Cadastro realizado com sucesso!\");
+					</script>
+					";
+			}else{
+	    		echo "Erro ao cadastrar";
+	    		$result->errorInfo();
+			}
 		}
 	?>
