ad7767da91c0d781b594beb500d65c8f8f0da067
NathalyAmaral <nathalyamaral07@gmail.com>
2019-03-23 12:44:20 -0300

    Cpf ja existente

--- App/Controller/emailExists.php
+++ App/Controller/emailExists.php
@@ -3,15 +3,25 @@
 	
 	include_once 'addCliente.php';
 
-	if(isset($_POST['email'])){ 
+	if(isset($_POST['email']) and isset($_POST['cpf'])){ 
 	 
 	    $email = $_POST['email'];
-	 	
+	 	$cpf = $_POST['cpf'];
+
 	 	$conexao = new Conexao();
         $conexao = $conexao->conexao();
         $stmt = $conexao->prepare('SELECT * FROM cliente WHERE email = "'.$email.'"');
         $stmt->execute();
+		var_dump($stmt);
+
+		$stmt2 = $conexao->prepare('SELECT * FROM cliente WHERE cpf = "'.$cpf.'"');
+        $stmt2->execute();
+        var_dump($stmt2);
+
+        $count2 = $stmt2->rowCount();
 		$count = $stmt->rowCount();
+		var_dump($count2, $count);
+		
 	    if($count > 0){
 	        echo "
 				<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=../../template_store/cadastro.php'>
@@ -19,7 +29,14 @@
 					alert(\"Email já existente, por favor digite outro!\");
 				</script>
 				";
-	    }else{
+	    }else if( $count2 > 0){
+	        echo "
+				<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=../../template_store/cadastro.php'>
+				<script type=\"text/javascript\">
+					alert(\"Cpf já existente, por favor digite outro!\");
+				</script>
+				";
+		}else{
 	    	addCliente($_POST);
 	    }
 	}
