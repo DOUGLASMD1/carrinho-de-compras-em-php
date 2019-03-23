6c6e78936eed16987aee8eb0d5fdaee0e5c197a1
NathalyAmaral <nathalyamaral07@gmail.com>
2019-03-23 14:50:27 -0300

    Listar seus produtos

--- App/Controller/ListController.php
+++ App/Controller/ListController.php
@@ -7,12 +7,9 @@ class ListController {
     public static function selectList() {
         $conexao = new Conexao();
         $conexao = $conexao->conexao();
-        $stmt = $conexao->prepare('SELECT produto.nome, produto.valor, produto.imagem, carrinho_has_produto.quantidade
-        FROM finalizacompra 
-        INNER JOIN carrinho ON carrinho.idcarrinho = finalizacompra.carrinho_idcarrinho 
-        INNER JOIN carrinho_has_produto ON carrinho.idcarrinho = carrinho_has_produto.carrinho_idcarrinho 
-        INNER JOIN produto ON produto.idproduto = carrinho_has_produto.produto_idproduto 
-        WHERE carrinho.cliente_cpf = "'.$_SESSION["user_cpf"].'" AND finalizaCompra.status = "CL";');
+        $stmt = $conexao->prepare('SELECT pedidoConcluido.nomeProduto, pedidoConcluido.valorProduto, pedidoConcluido.imagemProduto, pedidoConcluido.qtdProduto
+        FROM pedidoConcluido 
+        WHERE pedidoConcluido.cliente_cpf = "'.$_SESSION["user_cpf"].'";');
         $stmt->execute();
         $pCarrinho = $stmt->fetchAll();
         $stmt = null;
